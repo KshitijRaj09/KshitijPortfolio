8c3bf413d7e12673e62b11a26ce0c0a5ce758b35
Kshitij Raj <kshitijraj@MACPRO87.local>
2022-04-02 00:39:11 +0530

    Updated the portfolio

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const css = document.getElementById("second-css");
+import { mainSkills, familiarWithSkills } from './skills.js';
+
+const css = document.getElementById('second-css');
 const theme = document.querySelector('.theme-1');
 
-theme.addEventListener('click', ()=>{
-    if(!css.href.includes("css/darkmode.css")){
-        css.href="css/darkmode.css";
-    }
-
-    else{
-           css.href="";
-    }
+theme.addEventListener('click', () => {
+  if (!css.href.includes('css/darkmode.css')) {
+    css.href = 'css/darkmode.css';
+  } else {
+    css.href = '';
+  }
 });
 
-var date = new Date();
-const year = document.getElementById("year");
-year.innerHTML = date.getFullYear()+ " ";
+const date = new Date();
+const year = document.getElementById('year');
+year.innerHTML = date.getFullYear() + ' ';
+
+const renderSkillsinList = (skills) =>
+  skills.map((skill) => '<li>' + skill + '</li>').join('');
+
+const renderSkillsOnPage = () => {
+  const fetchMainSkills = document.querySelector('.main-skills');
+  const fetchFamiliarWithSkills = document.querySelector(
+    '.familiarwith-skills'
+  );
+  fetchMainSkills.innerHTML = renderSkillsinList(mainSkills);
+  fetchFamiliarWithSkills.innerHTML = renderSkillsinList(familiarWithSkills);
+};
+
+renderSkillsOnPage();
---
+++ skills.js
@@ -0,0 +1,16 @@
+export const mainSkills = [
+  'Reactjs',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React-Testing-Library',
+  'Jest',
+  'Version Control-Git',
+];
+
+export const familiarWithSkills = [
+  'TypeScript',
+  'Nodejs',
+  'Expressjs',
+  'MongoDB',
+];
